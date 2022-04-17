c45424b978b53b7994482299fd0b03c809ded31b
jrivero2020 <jrivero.2005@gmail.com>
2022-04-17 13:53:12 -0400

    Iconos centrados, popover y tooltip

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,17 +1,32 @@
 $(document).ready(function () {
 
+    // Scroll Suave ( smooth scroll.)
     $(".scrollSuave ul a").on("click", function (e) {
-        // 1
         e.preventDefault();
-        // 2
         const href = $(this).attr("href");
-
-        // 3
         $("html, body").animate({ scrollTop: $(href).offset().top }, 1200);
     });
+
+
+    // Funciones bootstrap jquery
+    // PopOver
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {return new bootstrap.Popover(popoverTriggerEl) })
+    
+    // toolTips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+
+
 }
 );
 
+// Función que coloca background a barra
+// de navegación cuando la página hace scroll
+// quita backgroud cuando está al comienzo
 let nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
